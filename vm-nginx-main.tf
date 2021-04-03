a894acc9687edd38b1125df8ac643003bf474226
idriss-m <idriss.mezguich@esprit.tn>
2021-04-03 10:50:20 +0100

    adding resource group definition and network resources

--- vm-nginx-main.tf
+++ vm-nginx-main.tf
@@ -7,6 +7,20 @@ resource "azurerm_resource_group" "network-rg" {
   name = var.resource-group
   location = var.location
 }
+
+resource "azurerm_virtual_network" "network-vnet" {
+  name = "network-vnet"
+  address_space = [var.network-vnet-cidr]
+  resource_group_name = azurerm_resource_group.network-rg.name
+  location = azurerm_resource_group.network-rg.location
+}
+# Create a subnet for VM
+resource "azurerm_subnet" "vm-subnet" {
+  name = "vm-subnet"
+  address_prefixes = [var.vm-subnet-cidr]
+  virtual_network_name = azurerm_virtual_network.network-vnet.name
+  resource_group_name  = azurerm_resource_group.network-rg.name
+}
 # Create Network Security Group
 resource "azurerm_network_security_group" "nginx-vm-nsg" {
   depends_on=[azurerm_resource_group.network-rg]
